7ac94863061513a5ca5eb35b78ef156a6e574d45
hoseinali <sinakhanjani@gmail.copm>
2018-07-08 03:16:32 +0430

    hosein

--- Kapp/View/SpendTableViewCell.swift
+++ Kapp/View/SpendTableViewCell.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class SpendTableViewCell: UITableViewCell {
 
+    // Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var factorIdLabel: UILabel!
+    @IBOutlet weak var bankNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var mountLabel: UILabel!
+    @IBOutlet weak var resultBankLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +29,23 @@ class SpendTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    
+    func configureCell(date:String, time:String, id:Int, bankName:String, status:String ,mount:String ,bankResponse:String) {
+        dateLabel.text = date
+        timeLabel.text = time
+        factorIdLabel.text = String(id)
+        bankNameLabel.text = bankName
+        mountLabel.text = mount
+        resultBankLabel.text = bankResponse
+        switch status {
+        case "0":self.statusLabel.text = "پرداخت نشده"
+        case "1":self.statusLabel.text = "پرداخت موفق"
+        case "2":self.statusLabel.text = "پرداخت ناموفق"
+        case "3":self.statusLabel.text = "لغو شده"
+        case "4":self.statusLabel.text = "رد شده"
+        default:self.statusLabel.text = "پرداخت نشده"
+        }
+        }
 
+    
 }
